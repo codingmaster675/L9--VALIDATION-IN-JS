fe44fc6fc10e52abbabe5c79f0132385fc416e69
codingmaster675 <topsecretshadowgovernment@gmail.com>
2024-11-23 14:54:57 +0300

    VALIDATION IN JS

---
+++ script.js
@@ -0,0 +1,11 @@
+function validate(){
+         var y=document.getElementById("Name").value;
+                  if (y == ""){
+           document.getElementById("name").innerHTML="Bior";
+            //alert("Please put your name")
+            return false;
+         };      
+         if(y.length < 5 ){
+            alert("Your name is less then 5 characters")
+         }
+};
